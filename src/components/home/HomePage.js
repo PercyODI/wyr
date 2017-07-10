6884621ca87a67637393ffbf13521a6dcb5083ee
Pearse Hutson <pearse.2008@gmail.com>
2017-07-10 13:10:20 -0500

    Initial commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  render(){
+    return (
+      <div className = "jumbotron">
+        <h1>TEST APP</h1>
+        <p>React, redux and react router in ES6 for ultra-responsive web apps. </p>
+        <p className = "btn btn-primary btn-lg">Learn More </p>
+      </div>
+    );
+  }
+}
+export default HomePage;
